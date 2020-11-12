a0710ed58881b68b23497f45717dc902f2cea6ec
Zygen <Xsaber64@Gmail.com>
2020-11-11 20:21:48 -0600

    TimesRepository.kt is borked QueryASyncTask

--- app/src/main/java/com/example/cmps359_logger_final_project/TimesRepository.kt
+++ app/src/main/java/com/example/cmps359_logger_final_project/TimesRepository.kt
@@ -2,9 +2,11 @@ package com.example.cmps359_logger_final_project
 import android.app.Application
 import android.os.AsyncTask
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.LiveData
 
 class TimesRepository(application: Application) {
+
+    private class MyTaskParams internal constructor(var time: Long, var id: Int, var username: String)
+
 
     val searchResults = MutableLiveData<List<Times>>()
 
@@ -15,12 +17,25 @@ class TimesRepository(application: Application) {
     private class QueryAsyncTask constructor(val asyncTaskDao: TimesDao?): AsyncTask<String, Void, List<Times>>() {
         var delegate: TimesRepository? = null
 
-        override fun doInBackground(vararg params: Int): List<Times>? {
-            return asyncTaskDao?.getTimes(params[0])
+        override fun doInBackground(vararg params: String): List<Times>? {
+            return asyncTaskDao?.getTimes(Integer.parseInt(params[0]))
         }
 
         override fun onPostExecute(result: List<Times>) {
             delegate?.asyncFinished(result)
+        }
+    }
+    private class InsertAsyncTask constructor(private val asyncTaskDao: TimesDao?) :
+        AsyncTask<Times, Void, Void>() {
+        override fun doInBackground(vararg params: Times): Void? {
+            asyncTaskDao?.insertTime(params[0])
+            return null
+        }
+    }
+    private class UpdateAsyncTask constructor(val asyncTaskDao: TimesDao?):
+            AsyncTask<MyTaskParams, Void, Void>(){
+        override fun doInBackground(vararg params: String?): Void {
+            return asyncTaskDao?.updateTimes(params[0])
         }
     }
 }
