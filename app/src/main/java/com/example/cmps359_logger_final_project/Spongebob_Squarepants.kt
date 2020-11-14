088bda98c6549dff0f2d5de6aad09b956a0cb913
Zygen <Xsaber64@Gmail.com>
2020-11-14 01:20:04 -0600

    Spongebob is done!

--- app/src/main/java/com/example/cmps359_logger_final_project/Spongebob_Squarepants.kt
+++ app/src/main/java/com/example/cmps359_logger_final_project/Spongebob_Squarepants.kt
@@ -1,10 +1,16 @@
 package com.example.cmps359_logger_final_project
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +41,85 @@ class Spongebob_Squarepants : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_spongebob__squarepants, container, false)
+    }
+
+    @SuppressLint("SetTextI18n", "Recycle")
+    override fun onStart() {
+        super.onStart()
+
+//        Get the splits table var
+        val tableLayout = view?.findViewById<TableLayout>(R.id.splitsTable)
+
+//        Query the DB to get user times
+        try {
+            var rank = 1
+            val curse = db!!.rawQuery(
+                "SELECT * FROM times WHERE gameId = 4 ORDER BY totalTime"
+                , null
+            )
+            val cindexname = curse.getColumnIndex("username")
+            val cindextime = curse.getColumnIndex("totalTime")
+            curse.moveToFirst()
+
+
+//            Create the table header rows
+            val tbrow0 = TableRow(context)
+            val tv0 = TextView(context)
+            tv0.text = " Rank "
+            tv0.setTextColor(Color.WHITE)
+            tbrow0.addView(tv0)
+            val tv1 = TextView(context)
+            tv1.text = " User "
+            tv1.setTextColor(Color.WHITE)
+            tbrow0.addView(tv1)
+            val tv2 = TextView(context)
+            tv2.text = " Time "
+            tv2.setTextColor(Color.WHITE)
+            tbrow0.addView(tv2)
+            tableLayout?.addView(tbrow0)
+
+
+
+
+//            populate the table first time
+            if (curse.count > 0) {
+                do {
+                    val time = curse.getLong(cindextime)
+                    val timeSecs = time.div(1000).rem(60) // convert to seconds
+                    val timeMins = time.div(1000 * 60).rem(60)  // get minutes
+                    val timeHrs = time.div(1000 * 60 * 60).rem(24) // get hours
+
+
+
+                    val tbrow = TableRow(context)
+                    val t1v = TextView(context)
+                    t1v.text = rank.toString()
+                    t1v.setTextColor(Color.WHITE)
+                    t1v.gravity = Gravity.CENTER
+                    tbrow.addView(t1v)
+
+                    val t2v = TextView(context)
+                    t2v.text = curse.getString(cindexname)
+                    t2v.setTextColor(Color.WHITE)
+                    t2v.gravity = Gravity.CENTER
+                    tbrow.addView(t2v)
+
+                    val t3v = TextView(context)
+                    t3v.text = "Time: [$timeHrs HRS $timeMins MINS $timeSecs SECS]"
+                    t3v.setTextColor(Color.WHITE)
+                    t3v.gravity = Gravity.CENTER
+                    tbrow.addView(t3v)
+
+                    tableLayout?.addView(tbrow)
+                    rank += 1
+
+//                    Continue to do so till we run out of data
+                } while (curse.moveToNext())
+            }
+//            Catch and print exceptions to the console
+        } catch (e: Exception) {
+            System.out.println(e.message)
+        }
     }
 
     companion object {
